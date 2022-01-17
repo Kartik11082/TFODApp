f8186ab6a18a7e2c08d5064d17fe91bdc1b9b8bd
Manish <manishjalui11@gmail.com>
2022-01-17 14:27:51 +0530

    Subtile text feature 0.1 is added

---
+++ src/components/home.js
@@ -0,0 +1,148 @@
+import React, { useRef, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+// 1. TODO - Import required model here
+// e.g. import * as tfmodel from "@tensorflow-models/tfmodel";
+import Webcam from "react-webcam";
+// 2. TODO - Import drawing utility here
+import { drawRect } from "../utilities";
+
+const Home = () => {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  // Main function
+  const runCoco = async () => {
+    // 3. TODO - Load network
+    // e.g. const net = await cocossd.load();
+    //https://hsrmodel.s3.jp-tok.cloud-object-storage.appdomain.cloud/model.json
+    const net = await tf.loadGraphModel(
+      "https://hsrmodel.s3.jp-tok.cloud-object-storage.appdomain.cloud/model.json"
+    );
+
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 16.7);
+  };
+
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // 4. TODO - Make Detections
+      const img = tf.browser.fromPixels(video);
+      const resized = tf.image.resizeBilinear(img, [640, 480]);
+      const casted = resized.cast("int32");
+      const expanded = casted.expandDims(0);
+      const obj = await net.executeAsync(expanded);
+
+      const boxes = await obj[2].array();
+      const classes = await obj[1].array();
+      const scores = await obj[3].array();
+      const guesstext = null;
+
+      // Draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+
+      // 5. TODO - Update drawing utility
+      // drawSomething(obj, ctx)
+      drawRect(
+        boxes[0],
+        classes[0],
+        scores[0],
+        0.9,
+        videoWidth,
+        videoHeight,
+        ctx,
+        guesstext
+      );
+
+      tf.dispose(img);
+      tf.dispose(resized);
+      tf.dispose(casted);
+      tf.dispose(expanded);
+      tf.dispose(obj);
+    }
+  };
+
+  useEffect(() => {
+    runCoco();
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <div
+		  style={{
+			background: "#333843",
+		  }}
+		> */}
+        <Webcam
+          ref={webcamRef}
+          muted={true}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+            borderRadius: 7,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+            borderRadius: 7,
+          }}
+        />
+        <p
+          id="p1"
+          style={{
+            position: "relative",
+            top: "265px",
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: 640,
+            height: 40,
+            background: "#121517",
+            // zindex: 10,
+            borderRadius: 5,
+          }}
+        ></p>
+        {/* </div> */}
+      </header>
+    </div>
+  );
+};
+
+export default Home;
--- src/utilities.js
+++ src/utilities.js
@@ -1,36 +1,61 @@
 // Define our labelmap
 const labelMap = {
-	1:{name:'a', color:'red'},
-	2:{name:'b', color:'yellow'},
-	3:{name:'c', color:'lime'},
-	4:{name:'d', color:'blue'},
-	5:{name:'e', color:'green'},
-	6:{name:'f', color:'pink'},
-	7:{name:'g', color:'white'},
-	8:{name:'h', color:'teal'},
-	9:{name:'i', color:'brown'},
-	10:{name:'j', color:'olive'},
-}
+  1: { name: "a", color: "red" },
+  2: { name: "b", color: "yellow" },
+  3: { name: "c", color: "lime" },
+  4: { name: "d", color: "blue" },
+  5: { name: "e", color: "green" },
+  6: { name: "f", color: "pink" },
+  7: { name: "g", color: "white" },
+  8: { name: "h", color: "teal" },
+  9: { name: "i", color: "brown" },
+  10: { name: "j", color: "black" },
+};
 
 // Define a drawing function
-export const drawRect = (boxes, classes, scores, threshold, imgWidth, imgHeight, ctx)=>{
-	for(let i=0; i<=boxes.length; i++){
-		if(boxes[i] && classes[i] && scores[i]>threshold){
-			// Extract variables
-			const [y,x,height,width] = boxes[i]
-			const text = classes[i]
-			
-			// Set styling
-			ctx.strokeStyle = labelMap[text]['color']
-			ctx.lineWidth = 10
-			ctx.fillStyle = 'white'
-			ctx.font = '30px Arial'         
-			
-			// DRAW!!
-			ctx.beginPath()
-			ctx.fillText(labelMap[text]['name'] + ' - ' + Math.round(scores[i]*100)/100, x*imgWidth, y*imgHeight-10)
-			ctx.rect(x*imgWidth, y*imgHeight, width*imgWidth/2, height*imgHeight/2);
-			ctx.stroke()
-		}
-	}
-}
+export const drawRect = (
+  boxes,
+  classes,
+  scores,
+  threshold,
+  imgWidth,
+  imgHeight,
+  ctx,
+  guesstext
+) => {
+  for (let i = 0; i <= boxes.length; i++) {
+    if (boxes[i] && classes[i] && scores[i] > threshold) {
+      // console.log(scores[i][1])
+      // console.log('go')
+      // Extract variables
+      const [y, x, height, width] = boxes[i];
+      const text = classes[i];
+
+      // Set styling
+      ctx.strokeStyle = labelMap[text]["color"];
+      ctx.lineWidth = 5;
+      ctx.fillStyle = labelMap[text]["color"];
+      ctx.font = "28px Arial";
+
+      // DRAW!!
+      ctx.beginPath();
+      ctx.fillText(
+        labelMap[text]["name"] + " - " + Math.round(scores[i] * 100) / 100,
+        x * imgWidth,
+        y * imgHeight - 10
+      );
+      ctx.rect(
+        x * imgWidth,
+        y * imgHeight,
+        (width * imgWidth) / 2,
+        (height * imgHeight) / 1.5
+      );
+      ctx.stroke();
+
+      guesstext = labelMap[text]["name"];
+      console.log(guesstext);
+      document.getElementById("p1").innerHTML =
+        document.getElementById("p1").innerHTML + guesstext;
+    }
+  }
+};
